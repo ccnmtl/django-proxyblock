ac4691a95a2cb6e6bbff76a5e0a147eb70f70d96
Anders Pearson <anders@columbia.edu>
2016-04-21 10:31:58 +0100

    release 0.2.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-proxyblock",
-    version="0.1.2",
+    version="0.2.0",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
